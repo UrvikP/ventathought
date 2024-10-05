99ba1ab7eca0e9c37bce65bf8ac6f1118a64857c
msalaheen01 <mus2004@nyu.edu>
2024-10-05 03:19:56 -0400

    auth
    initial authentication commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,38 @@
+'use client'
+import { Box, Button, Typography } from "@mui/material";
+import { useUser } from '@auth0/nextjs-auth0/client';
 import Image from "next/image";
 
+
+
 export default function Home() {
+  const {user, error, isLoading} = useUser();
+
+  if (isLoading) {
+    return <Typography>Loading...</Typography>; // Show a loading message while checking authentication
+  }
+
+  if (!user) {
+    window.location.href = '/api/auth/login';
+  }
+
   return (
-    <div>Nothing here</div>
+    <Box>
+      <Typography variant="h2" component="h1" gutterBottom sx={{ mb: 4, textAlign: 'center' }}>
+                    Text
+      </Typography>
+                  <Box display="flex" justifyContent="center">
+                  <Button color="inherit" href="/api/auth/logout">
+            logout
+          </Button>
+                  <Button 
+                    href="/api/auth/login" 
+                    variant="contained" 
+                  
+                  >
+                    login
+                  </Button>
+                  </Box>
+    </Box>
   )
 }
