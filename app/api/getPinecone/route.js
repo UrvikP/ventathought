5a06e5556cd26db0bc4ef41d272fc35575f826e8
it2304 <it2304@columbia.edu>
2024-10-05 13:54:13 -0400

    Fixed chat endpoints

--- app/api/getPinecone/route.js
+++
@@ -1,9 +0,0 @@
-import { Pinecone } from "@pinecone-database/pinecone"
-
-export async function POST(req) {
-    const pc = new Pinecone({
-        apiKey: process.env.PINECONE_API_KEY,
-    });
-
-    return pc
-}
